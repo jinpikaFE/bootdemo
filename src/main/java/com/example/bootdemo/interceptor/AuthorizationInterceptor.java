0567f4b9a66765ed058a797b38fe4029117931dd
jinxin <jinxin@hsmap.com>
2022-11-22 17:15:15 +0800

    feat: 简单的token验证

--- src/main/java/com/example/bootdemo/interceptor/AuthorizationInterceptor.java
+++ src/main/java/com/example/bootdemo/interceptor/AuthorizationInterceptor.java
@@ -46,8 +46,8 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
         // 如果打上了AuthToken注解则需要验证token
         if (method.getAnnotation(AuthToken.class) != null || handlerMethod.getBeanType().getAnnotation(AuthToken.class) != null) {
 
-//            String token = request.getHeader(httpHeaderName);
-            String token = request.getParameter(httpHeaderName);
+            String token = request.getHeader(httpHeaderName);
+//            String token = request.getParameter(httpHeaderName);
             log.info("Get token from request is {} ", token);
             String username = "";
             Jedis jedis = new Jedis("127.0.0.1", 6379);
